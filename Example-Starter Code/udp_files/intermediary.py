43d0fd3947b3f5e36a3cb356d5f8b18f2dce778e
Ben Wodka <bmwodka@protonmail.com>
2024-02-13 13:32:38 -0600

    can add now

--- Example-Starter Code/udp_files/intermediary.py
+++ Example-Starter Code/udp_files/intermediary.py
@@ -3,3 +3,8 @@ from supabase import create_client, Client
 url: str = "https://blzwcpdxyfmqngexhskf.supabase.co" 
 key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJsendjcGR4eWZtcW5nZXhoc2tmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDcyNTEyMTMsImV4cCI6MjAyMjgyNzIxM30.Y78DCDzwlRNW8MVQiVJ4itxl9NdjV99PPa7Q9hh_daI"  
 supabase: Client = create_client(url, key)
+
+
+new_record = {"id": "123", "codename": "test"}
+inserted_data = supabase.table("player").insert(new_record).execute()
+print(inserted_data.data)  # Prints the inserted data
