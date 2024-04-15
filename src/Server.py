5d04d228232a552a3760f45647c039a4e1094ab9
amwells-git <amwells2021@gmail.com>
2024-04-15 10:27:10 -0500

    Updated UDP transmit to accept given transmit port rather than only transmitting across local IP

--- src/Server.py
+++ src/Server.py
@@ -3,7 +3,7 @@ import socket
 
 class UDPServer:
     # ports are (IP, port) tuple format, defaults to local ports 7500 for transmit and 7501 for receive
-    def __init__(self, transmit_port=("127.0.0.1", 7500), receive_port=("127.0.0.1", 7501)):
+    def __init__(self, transmit_port=('127.0.0.1', 7500), receive_port=('127.0.0.1', 7501)):
         self.transmit_port = transmit_port
         self.receive_port = receive_port
         self.receive_socket = self.create_udp_socket(self.receive_port)
@@ -15,7 +15,7 @@ class UDPServer:
         return udp_socket
 
     def transmit_message(self, message):
-        self.receive_socket.sendto(message.encode(), ('127.0.0.1', 7500))
+        self.receive_socket.sendto(message.encode(), self.transmit_port)
         print(f"Server transmitted (sent) message '{message}' to {7500}")
 
     def receive_message(self, buffer_size=1024):
